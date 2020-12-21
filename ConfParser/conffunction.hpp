68392d83290d60a13af88af54dc18917151a8fb9
Kilian-Jugie <kilian.jugie@viacesi.fr>
2020-12-21 23:49:36 +0100

    Updated copyright

--- ConfParser/conffunction.hpp
+++ ConfParser/conffunction.hpp
@@ -1,3 +1,9 @@
+/*
+* Copyright (C) 2020 Kilian Jugie - All Rights Reserved
+* Unauthorized copying of this file, via any medium is strictly prohibited
+* Proprietary and confidential
+*/
+
 #pragma once
 #include "global.hpp"
 #include "confscope.hpp"
--- ConfParser/confinstance.cpp
+++ ConfParser/confinstance.cpp
@@ -1,3 +1,9 @@
+/*
+* Copyright (C) 2020 Kilian Jugie - All Rights Reserved
+* Unauthorized copying of this file, via any medium is strictly prohibited
+* Proprietary and confidential
+*/
+
 #include "confinstance.hpp"
 #include "conftype.hpp"
 #include "conffunction.hpp"
